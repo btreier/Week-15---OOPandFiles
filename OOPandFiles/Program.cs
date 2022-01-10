e597d6ed40895d0ec98980ba47a4fef9f5a139dd
Birgith Maria Treier <btreier@tktk.ee>
2022-01-10 12:53:45 +0200

    add new projects

---
+++ OOPandFiles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPandFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
